9f8ca2a29c1a58d193244c8bb767f8f1996114da
NguyenDuy <nguyenduy27092002@gmail.com>
2021-04-01 17:29:06 +0700

    Add code of list page (part 1)

---
+++ resources/views/frontend/list.blade.php
@@ -0,0 +1,176 @@
+@extends('layout.app')
+@section('content')
+    <link rel="stylesheet" href="{{asset('asset/css/list.css')}}">
+    <style>
+        .bg-image{
+            background-image: url("{{asset('asset/img/list-page/category.png')}}");
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+    </style>
+    <div class="container">
+        <h3 class="text-white bg-info p-2 my-5"><a href="#" class="link-style-none text-white">Laptop</a> >> Laptop Gaming</h3>
+        <div class="row mb-3">
+            <div class="col-12 col-sm-12 col-md-3 col-lg-3 mb-3">
+                <div class="p-1">
+                    <a class="link-style-none" href="#">
+                        <img src="{{asset('asset/img/list-page/category.png')}}" class="img-fluid w-100 border-radius">
+                        <div class="p-2 bg-light">
+                            <h4>Laptop HP Gaming Pavilion...</h4>
+                            <p class="text-danger mb-1">15.800.000 đ</p>
+                            <small class="text-secondary">18.800.000 đ</small>
+                            <p><samll><img src="{{asset('asset/img/list-page/star.png')}}"> 5000 đánh giá</samll></p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-12 col-sm-12 col-md-3 col-lg-3 mb-3">
+                <div class="p-1">
+                    <a class="link-style-none" href="#">
+                        <img src="{{asset('asset/img/list-page/category.png')}}" class="img-fluid w-100 border-radius">
+                        <div class="p-2 bg-light">
+                            <h4>Laptop HP Gaming Pavilion...</h4>
+                            <p class="text-danger mb-1">15.800.000 đ</p>
+                            <small class="text-secondary">18.800.000 đ</small>
+                            <p><samll><img src="{{asset('asset/img/list-page/star.png')}}"> 5000 đánh giá</samll></p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-12 col-sm-12 col-md-3 col-lg-3 mb-3">
+                <div class="p-1">
+                    <a class="link-style-none" href="#">
+                        <img src="{{asset('asset/img/list-page/category.png')}}" class="img-fluid w-100 border-radius">
+                        <div class="p-2 bg-light">
+                            <h4>Laptop HP Gaming Pavilion...</h4>
+                            <p class="text-danger mb-1">15.800.000 đ</p>
+                            <small class="text-secondary">18.800.000 đ</small>
+                            <p><samll><img src="{{asset('asset/img/list-page/star.png')}}"> 5000 đánh giá</samll></p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-12 col-sm-12 col-md-3 col-lg-3 mb-3">
+                <div class="p-1">
+                    <a class="link-style-none" href="#">
+                        <img src="{{asset('asset/img/list-page/category.png')}}" class="img-fluid w-100 border-radius">
+                        <div class="p-2 bg-light">
+                            <h4>Laptop HP Gaming Pavilion...</h4>
+                            <p class="text-danger mb-1">15.800.000 đ</p>
+                            <small class="text-secondary">18.800.000 đ</small>
+                            <p><samll><img src="{{asset('asset/img/list-page/star.png')}}"> 5000 đánh giá</samll></p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="row mb-3">
+            <div class="col-12 col-sm-12 col-md-3 col-lg-3 mb-3">
+                <div class="p-1">
+                    <a class="link-style-none" href="#">
+                        <img src="{{asset('asset/img/list-page/category.png')}}" class="img-fluid w-100 border-radius">
+                        <div class="p-2 bg-light">
+                            <h4>Laptop HP Gaming Pavilion...</h4>
+                            <p class="text-danger mb-1">15.800.000 đ</p>
+                            <small class="text-secondary">18.800.000 đ</small>
+                            <p><samll><img src="{{asset('asset/img/list-page/star.png')}}"> 5000 đánh giá</samll></p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-12 col-sm-12 col-md-3 col-lg-3 mb-3">
+                <div class="p-1">
+                    <a class="link-style-none" href="#">
+                        <img src="{{asset('asset/img/list-page/category.png')}}" class="img-fluid w-100 border-radius">
+                        <div class="p-2 bg-light">
+                            <h4>Laptop HP Gaming Pavilion...</h4>
+                            <p class="text-danger mb-1">15.800.000 đ</p>
+                            <small class="text-secondary">18.800.000 đ</small>
+                            <p><samll><img src="{{asset('asset/img/list-page/star.png')}}"> 5000 đánh giá</samll></p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-12 col-sm-12 col-md-3 col-lg-3 mb-3">
+                <div class="p-1">
+                    <a class="link-style-none" href="#">
+                        <img src="{{asset('asset/img/list-page/category.png')}}" class="img-fluid w-100 border-radius">
+                        <div class="p-2 bg-light">
+                            <h4>Laptop HP Gaming Pavilion...</h4>
+                            <p class="text-danger mb-1">15.800.000 đ</p>
+                            <small class="text-secondary">18.800.000 đ</small>
+                            <p><samll><img src="{{asset('asset/img/list-page/star.png')}}"> 5000 đánh giá</samll></p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-12 col-sm-12 col-md-3 col-lg-3 mb-3">
+                <div class="p-1">
+                    <a class="link-style-none" href="#">
+                        <img src="{{asset('asset/img/list-page/category.png')}}" class="img-fluid w-100 border-radius">
+                        <div class="p-2 bg-light">
+                            <h4>Laptop HP Gaming Pavilion...</h4>
+                            <p class="text-danger mb-1">15.800.000 đ</p>
+                            <small class="text-secondary">18.800.000 đ</small>
+                            <p><samll><img src="{{asset('asset/img/list-page/star.png')}}"> 5000 đánh giá</samll></p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="row mb-3">
+            <div class="col-12 col-sm-12 col-md-3 col-lg-3 mb-3">
+                <div class="p-1">
+                    <a class="link-style-none" href="#">
+                        <img src="{{asset('asset/img/list-page/category.png')}}" class="img-fluid w-100 border-radius">
+                        <div class="p-2 bg-light">
+                            <h4>Laptop HP Gaming Pavilion...</h4>
+                            <p class="text-danger mb-1">15.800.000 đ</p>
+                            <small class="text-secondary">18.800.000 đ</small>
+                            <p><samll><img src="{{asset('asset/img/list-page/star.png')}}"> 5000 đánh giá</samll></p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-12 col-sm-12 col-md-3 col-lg-3 mb-3">
+                <div class="p-1">
+                    <a class="link-style-none" href="#">
+                        <img src="{{asset('asset/img/list-page/category.png')}}" class="img-fluid w-100 border-radius">
+                        <div class="p-2 bg-light">
+                            <h4>Laptop HP Gaming Pavilion...</h4>
+                            <p class="text-danger mb-1">15.800.000 đ</p>
+                            <small class="text-secondary">18.800.000 đ</small>
+                            <p><samll><img src="{{asset('asset/img/list-page/star.png')}}"> 5000 đánh giá</samll></p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-12 col-sm-12 col-md-3 col-lg-3 mb-3">
+                <div class="p-1">
+                    <a class="link-style-none" href="#">
+                        <img src="{{asset('asset/img/list-page/category.png')}}" class="img-fluid w-100 border-radius">
+                        <div class="p-2 bg-light">
+                            <h4>Laptop HP Gaming Pavilion...</h4>
+                            <p class="text-danger mb-1">15.800.000 đ</p>
+                            <small class="text-secondary">18.800.000 đ</small>
+                            <p><samll><img src="{{asset('asset/img/list-page/star.png')}}"> 5000 đánh giá</samll></p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div class="col-12 col-sm-12 col-md-3 col-lg-3 mb-3">
+                <div class="p-1">
+                    <a class="link-style-none" href="#">
+                        <img src="{{asset('asset/img/list-page/category.png')}}" class="img-fluid w-100 border-radius">
+                        <div class="p-2 bg-light">
+                            <h4>Laptop HP Gaming Pavilion...</h4>
+                            <p class="text-danger mb-1">15.800.000 đ</p>
+                            <small class="text-secondary">18.800.000 đ</small>
+                            <p><samll><img src="{{asset('asset/img/list-page/star.png')}}"> 5000 đánh giá</samll></p>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection

715312feba618276e2ae0aa0e0ffc717eda747c9
NguyenDuy <nguyenduy27092002@gmail.com>
2021-04-03 22:22:24 +0700

    Fixed border-radius, change some words and swap plus and minus sign positions

--- resources/views/frontend/cart.blade.php
+++ resources/views/frontend/cart.blade.php
@@ -3,9 +3,9 @@
     <link rel="stylesheet" href="{{asset('asset/css/cart.css')}}">
     <div class="container">
         @for($i=1; $i<=3; $i++)
-            <div class="row my-5 box-shadow">
+            <div class="row my-5 box-shadow border-radius">
                 <div class="col-12 col-sm-12 col-md-4 col-lg-4">
-                    <img src="{{asset('asset/img/cart-page/laptop-lenovo.png')}}" class="img-fluid w-100">
+                    <img src="{{asset('asset/img/cart-page/laptop-lenovo.png')}}" class="img-fluid w-100 border-radius p-2">
                 </div>
                 <div class="col-12 col-sm-12 col-md-8 col-lg-8 p-4">
                     <h5 class="text-uppercase">laptop lenovo legion 5 15imh05 (82au004xvn) geforce gtx1650 4gb intel corei5 10300h 8gb 512 gb 15.6" 120hz ips rgb win 10</h5>
@@ -16,14 +16,14 @@
                     <h5>Số lượng</h5>
                     <div class="input-group flex-wrap text-center">
                         <div class="input-group-prepend">
-                            <button type="button" class="btn btn-outline-primary rounded-0 increment incPrice">
-                                <i class="fas fa-plus"></i>
+                            <button type="button" class="btn btn-outline-primary bg-info rounded-0 decrement decPrice">
+                                <i class="fas fa-minus text-white"></i>
                             </button>
                         </div>
                         <input type="number" class="quantity counter_increment counter_decrement" value="1">
                         <div class="input-group-append">
-                            <button type="button" class="btn btn-outline-primary rounded-0 decrement decPrice">
-                                <i class="fas fa-minus"></i>
+                            <button type="button" class="btn btn-outline-primary bg-info rounded-0 increment incPrice">
+                                <i class="fas fa-plus text-white"></i>
                             </button>
                         </div>
                         <span class="h4 text-primary ml-5 total_price">41.980.000</span>
@@ -35,7 +35,7 @@
                 </div>
             </div>
         @endfor
-        <div class="row mb-5 box-shadow p-3">
+        <div class="row mb-5 box-shadow p-3 border-radius">
             <div class="col-12 col-sm-12 col-md-6 col-lg-6 mb-3">
                 <h3 class="mb-5">Thanh toán</h3>
                 <div class="row">
@@ -56,7 +56,7 @@
                 </div>
             </div>
             <div class="col-12 col-sm-12 col-md-6 col-lg-6 mb-3">
-                <h3 class="mb-5">Chọn phương thức thanh toán</h3>
+                <h3 class="mb-5">Chấp nhận thanh toán</h3>
                 <div class="row bg-light-blue p-4 box-shadow border-radius">
                     <div class="col-4">
                         <a class="link-style-none" href="#">
@@ -82,7 +82,7 @@
                 <button type="submit" class="btn btn-primary border-radius pay">Tiến hành thanh toán</button>
             </div>
         </div>
-        <div class="row mb-5 box-shadow p-3 hidden">
+        <div class="row mb-5 box-shadow p-3 hidden border-radius">
             <form action="#" method="post" class="w-100">
                 <div class="row form-group">
                     <div class="col-12 col-sm-12 col-md-6 col-lg-6 mb-3">

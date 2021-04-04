67bbeeb618f139a045f5a031efcd8909c2c737d8
dangkhanh <dangk13122002@gmail.com>
2021-04-04 12:21:51 +0700

    fix some error

--- app/Models/Tags.php
+++ app/Models/Tags.php
@@ -36,7 +36,7 @@ class Tags extends Model
     */
 
     public function products(){
-        return $this->belongsToMany(Product::class,'pivot','product_id','tag_id');
+        return $this->belongsToMany(Product::class,'pivot','tag_id','product_id');
     }
 
 //    public function parent(){

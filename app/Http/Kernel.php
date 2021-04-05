4f725223a263c5129ec994ef1c0f87689da8a678
Phamquanglinh <Linh2002gv@gmail.com>
2021-04-05 08:38:51 +0700

    add boss to kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -54,6 +54,7 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
+        'boss' => \App\Http\Middleware\CheckIfBoss::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'cache.headers' => \Illuminate\Http\Middleware\SetCacheHeaders::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,

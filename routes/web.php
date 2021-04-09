ac2d4e86f0659d1d2e9120f60c46f78e58dcdd5a
Phamquanglinh <Linh2002gv@gmail.com>
2021-04-09 21:09:17 +0700

    fix conflic

--- routes/web.php
+++ routes/web.php
@@ -35,8 +35,6 @@ Route::get('/product/{slug}', [\App\Http\Controllers\Frontend\ProductController:
 Route::get('/cart/{method}/{id}', [\App\Http\Controllers\Frontend\CartController::class, 'ajax', 'method', 'id'])->where(['method', 'id'])->name('cart.ajax');
 Route::get('/cart/{method}/{product_id}/{quantity}', [\App\Http\Controllers\Frontend\CartController::class, 'addToCart', 'method', 'product_id', 'quantity'])->where(['method', 'product_id', 'quantity'])->name('cart.add');
 Route::get('/cart-remove/remove/{id}', [\App\Http\Controllers\Frontend\CartController::class, 'removeItem', 'id'])->where(['id'])->name('cart.remove');
-<<<<<<< HEAD
+
 Route::get('/search',[\App\Http\Controllers\Frontend\SearchController::class,'getData'])->name('search');
-=======
 Route::get('/page/{key}', [\App\Http\Controllers\Frontend\FixedPageController::class, 'index','key'])->where(['key'])->name('frontend.page');
->>>>>>> 2687903e33fb7cc50483fc8c5cc47bce4aaca8fc

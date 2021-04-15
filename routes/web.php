dcf1e909e7b7d60728ba582de2252394fbea619a
Phamquanglinh <Linh2002gv@gmail.com>
2021-04-15 16:14:26 +0700

    make controller for index

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('frontend.index');
-})->name('index');
+Route::get('/',[\App\Http\Controllers\IndexController::class,'getData'])->name('index');
 Route::get('/contact', function () {
     return view('frontend.contact');
 })->name('frontend.contact');
